d99d3d1540bacee9511c534577ace0f4633859f1
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-24 15:03:36 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  ARM_CLIENT_SECRET = "QZV=A]JAMtzQ.WUQi.bwXnuedYjl3973"
-  ARM_CLIENT_ID = "33764c29-a12d-44f7-8c8c-e871dab11de6"
-  ARM_TENANT_ID = "513294a0-3e20-41b2-a970-6d30bf1546fa"
-  ARM_SUBSCRIPTION_ID = "2c7688d7-92df-4b63-bc3f-e91ed4b6d4af"
+  client_secret = "QZV=A]JAMtzQ.WUQi.bwXnuedYjl3973"
+  client_id = "33764c29-a12d-44f7-8c8c-e871dab11de6"
+  tenant_id = "513294a0-3e20-41b2-a970-6d30bf1546fa"
+  subscription_id = "2c7688d7-92df-4b63-bc3f-e91ed4b6d4af"
 }
