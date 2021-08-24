a05def0f768dcf70f38667c5aae47d42f0885fb2
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-24 15:34:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,25 @@ resource "azurerm_resource_group" "hostterraformgroup" {
     }
 }
 
+# Create virtual network
+resource "azurerm_virtual_network" "hostterraformnetwork" {
+    name                = var.vnet_name
+    address_space       = ["10.0.0.0/16"]
+    location            = "Central India"
+    resource_group_name = azurerm_resource_group.hostterraformgroup.name
 
+    tags = {
+        environment = "Terraform Host"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "hostterraformsubnet" {
+    name                 = var.subnet_name
+    resource_group_name  = azurerm_resource_group.hostterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.hostterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
 # Create public IPs
 resource "azurerm_public_ip" "hostterraformpublicip" {
     name                         = var.public_ip_name
