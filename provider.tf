1aa64b703778fe790a928e433743bc6c869580b7
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-24 14:50:59 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,12 @@
-provider "azurerm" {
-    features {}
-}
-
-
+# Configure the Microsoft Azure Provider
 terraform {
-  backend "azurerm" {
-
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
   }
+}
+provider "azurerm" {
+  features {}
 }
