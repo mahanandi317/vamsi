df35197028918240dd77695a575e4a4ebe13a6fe
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-24 14:46:34 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,9 +5,6 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    // resource_group_name   = var.resource_group_name_02
-    // storage_account_name  = var.storage_account_02
-    // container_name        = var.container_name_02
-    // key                   = var.state_key_02
+
   }
 }
