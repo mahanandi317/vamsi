8f2ac44e542b4020448d660d0c084758d65cae38
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-24 14:44:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ provider "azurerm" {
     # If you're using version 1.x, the "features" block is not allowed.
     version = "~>2.0"
     features {}
-    ARM_CLIENT_SECRET = 'QZV=A]JAMtzQ.WUQi.bwXnuedYjl3973'
-    ARM_CLIENT_ID = '33764c29-a12d-44f7-8c8c-e871dab11de6'
-    ARM_TENANT_ID = '513294a0-3e20-41b2-a970-6d30bf1546fa'
-    ARM_SUBSCRIPTION_ID = '2c7688d7-92df-4b63-bc3f-e91ed4b6d4af'
 }
 
 # Create a resource group if it doesn't exist
