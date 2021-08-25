1520d1ed3b2f5a3fba6cd549522ea77a9a338ad7
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-25 17:28:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "cloudinit_config" "foo" {
   part {
     content_type = "text/x-shellscript"
     content = "baz"
-    filename = "text.sh"
+    filename = "text1.sh"
   }
 }
 resource "azurerm_resource_group" "hostterraformgroup" {
