48b44ba7f26f1cd19e509c662421c0766e98f9a2
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-25 14:42:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine" "hostterraformvm" {
     tags = {
         environment = "Terraform Host"
     }
-    custom_data = "cloud_init.txt"
+    custom_data = cloud_init.txt
 }
 
 
