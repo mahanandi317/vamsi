1b221c325cdc09e5d0527f37a5490e637984bb32
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-25 16:13:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,8 @@ resource "azurerm_network_security_group" "hostterraformnsg" {
     location            = "Central India"
     resource_group_name = azurerm_resource_group.hostterraformgroup.name
 
-    security_rule {
+    custom_rules = [ 
+      {
         name                       = "SSH"
         priority                   = 1001
         direction                  = "Inbound"
@@ -68,8 +69,19 @@ resource "azurerm_network_security_group" "hostterraformnsg" {
         destination_port_range     = "22"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
+    },
+    {
+      name                    = "myhttp"
+      priority                = 200
+      direction               = "Inbound"
+      access                  = "Allow"
+      protocol                = "tcp"
+      source_port_range       = "*"
+      destination_port_range  = "8080"
+      source_address_prefixes = ["10.151.0.0/24", "10.151.1.0/24"]
+      description             = "description-http"
     }
-    
+    ]
     tags = {
         environment = "Terraform Host"
     }
