a8ab6074a90850e0c87c8d3709e6bf66341da4e6
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-25 06:26:19 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,5 +13,4 @@ provider "azurerm" {
   client_id = "3deb656c-1ee3-4a02-842f-1bad58d12f01"
   tenant_id = "eeeb6497-738c-49c7-b0cd-68cd0669e13d"
   subscription_id = "a680f040-a5db-4a85-b40f-61bf75c5aa4c"
-
 }
