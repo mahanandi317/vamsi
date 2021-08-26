bcf6b1674133456acbb3cce09ffdd12aee490031
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-26 05:24:01 +0000

    Update text1.sh

--- text1.sh
+++ text1.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
-                      sudo add-apt-repository "deb [arch=amd64] https://downler.oad.dockcom/linux/ubuntu bionic stable"
-                      sudo curl -sSL https://get.docker.com/ | sh
-                      sudo systemctl start docker
-                      sudo systemctl enable docker
-                      sudo docker pull nginx
-                      sudo docker run --name docker-nginx-23 -p 80:80 nginx
-                      sudo docker ps -a
+sudo apt install docker.io -y
+sudo systemctl start docker 
+sudo docker pull nginx
+sudo docker images
+sudo docker run --name docker-nginx-23 -p 80:80 -d nginx
+sudo docker ps -a
+          
