0c7b65a7a5e249d857151cd086c2d89f88c4f2e5
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-26 05:45:36 +0000

    Update text1.sh

--- text1.sh
+++ text1.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt install docker.io -y
-sudo systemctl start docker 
-sudo docker pull nginx
-sudo docker images
-sudo docker run --name docker-nginx-23 -p 80:80 -d nginx
-sudo docker ps -a
-          
+#sudo apt-get update
+#sudo apt install docker.io -y
+#sudo systemctl start docker 
+#sudo docker pull nginx
+#sudo docker images
+#sudo docker run --name docker-nginx-23 -p 80:80 -d nginx
+#sudo docker ps -a
+touch /tmp/test
