940d5c75cb164370dee1683e65e5da3ba7c10637
sneharao-06 <89451526+sneharao-06@users.noreply.github.com>
2021-08-26 07:31:07 +0000

    Delete text1.sh

--- text1.sh
+++
@@ -1,8 +0,0 @@
-#sudo apt-get update
-#sudo apt install docker.io -y
-#sudo systemctl start docker 
-#sudo docker pull nginx
-#sudo docker images
-#sudo docker run --name docker-nginx-23 -p 80:80 -d nginx
-#sudo docker ps -a
-touch /tmp/test
