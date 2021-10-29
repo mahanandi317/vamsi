055247699a903230ba073d9778f317c7cc149264
userazure12 <91203797+userazure12@users.noreply.github.com>
2021-10-29 21:02:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_secret = "Na7B4ech3y58to9OVC_x5yPNm-~6w.IgwT"
+  client_secret = "6So7Q~ZhXM7ZqXUT2FmTlifqQUaiA-FkHK9X5"
   client_id = "3deb656c-1ee3-4a02-842f-1bad58d12f01"
   tenant_id = "eeeb6497-738c-49c7-b0cd-68cd0669e13d"
   subscription_id = "a680f040-a5db-4a85-b40f-61bf75c5aa4c"
