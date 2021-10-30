8e6f71acc2311f979718a8936e23ab829a8c2967
mahanandi317 <92777135+mahanandi317@users.noreply.github.com>
2021-10-30 19:12:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features { }
-  client_secret = "1BI7Q~8nkUxOlnvG0lXergTM.4M5h4QSDJkVH"
-  client_id = "0a123ec8-46a3-4a31-9e10-1ad502657015"
+  client_secret = "v4u7Q~knOng1DSYPpHjKv0Am9C.65mKWcNh_v"
+  client_id = "c2637892-b0b2-464e-a9ae-0ea854e4c7ef"
   tenant_id = "eeeb6497-738c-49c7-b0cd-68cd0669e13d"
   subscription_id = "a680f040-a5db-4a85-b40f-61bf75c5aa4c"
 }
